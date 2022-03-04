5396a6697e3e5ed8b5e01edac2e9fadb44eacb87
Christian Thompson <ChristianThompson1@hotmail.com>
2022-03-04 12:21:24 -0800

    changed file structure

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 """Main script, uses other modules to generate sentences."""
-from flask import Flask
+from flask import Flask, render_template, flash, redirect, request
+from markov import MarkovbutBetter
+from tokens import tokenize
+import twitter
 
 
 app = Flask(__name__)
@@ -9,12 +12,18 @@ app = Flask(__name__)
 def before_first_request():
     """Runs only once at Flask startup"""
     # TODO: Initialize your histogram, hash table, or markov chain here.
+    tokens = tokenize('sample.txt')
+    mark = MarkovbutBetter(tokens)
+
 
 
 @app.route("/")
-def home():
-    """Route that returns a web page containing the generated text."""
-    return "<p>TODO: Return a word here!</p>"
+def tweet():
+    sentence = request.form['sentence']
+    print(sentence)
+    status = twitter.tweet(sentence)
+    print(status)
+    return redirect('/')
 
 
 if __name__ == "__main__":
